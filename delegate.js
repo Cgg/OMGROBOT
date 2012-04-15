d57f2b9d0e23874e181347261e4e39f1ed13af6b
Cgg <clement.geiger@gmail.com>
2012-04-15 12:21:46 +0200

    delegate now with parameters

--- delegate.js
+++ delegate.js
@@ -1,4 +1,16 @@
 function Delegate( obj, objMethod )
 {
-  return function(){ return objMethod.call( obj ); };
+  if( arguments.length > 2 )
+  {
+    var _params = [];
+    for( var n = 2; n < arguments.length; ++n )
+    {
+      _params.push(arguments[n]);
+    }
+    return function() { return objMethod.apply( obj, _params ); }
+  }
+  else
+  {
+    return function() { return objMethod.call( obj ); }
+  }
 }
