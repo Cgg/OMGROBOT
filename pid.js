cf25629244404da91dcd302929ef5feaa765b253
Clement Geiger <clement.geiger@gmail.com>
2012-05-05 20:44:55 +0200

    updated comments at the top of the file

--- pid.js
+++ pid.js
@@ -1,12 +1,17 @@
 /* PID
  *
- * Encapsulates a sampled PID command. Takes the three parameters at creation,
- * plus an initial command.
+ * Encapsulates a sampled PID controller. Takes the three parameters at creation,
+ * plus initial command and feedback.
  *
  * The reference command can then be set to other values.
  *
  * To obtain the output command, you have to provide the object with a feedback,
  * via the updateFeedback method.
+ *
+ * The variable names ending with an "N" denote a variable sampled at each step.
+ *
+ * See http://www.rcva.fr/index.php?option=com_content&view=article&id=27&Itemid=42&limitstart=8
+ * for more information about the controller.
  */
 
 function PID( P, I, D, initReference )
