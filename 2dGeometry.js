d676e4fc3fdd1fbe3cf5dd11b7b501c1f14cba78
Cgg <clement.geiger@gmail.com>
2014-07-04 16:47:33 +0200

    remove dead stuff

--- 2dGeometry.js
+++ 2dGeometry.js
@@ -4,8 +4,6 @@
  *
  * For now, contains
  *  - Point
- *  - Vector
- *
  */
 
 /* class Point
@@ -18,54 +16,6 @@ function Point( X, Y )
   this.x = X;
   this.y = Y;
 }
-
-
-/* class Vector
- *
- * A Vector instance is immutable, that is V1.Add( V2 ) wont modify V1 but
- * return V3 = V1 + V3 instead.
- */
-
-Vector.CreateFromPoints = function( P1, P2 )
-{
-  return new Vector( P2.x - P1.x, P2.y - P1.y );
-};
-
-function Vector( X, Y )
-{
-  this.x = X;
-  this.y = Y;
-}
-
-Vector.prototype.isNull = function()
-{
-  return ( this.x === 0 ) && ( this.y === 0 );
-};
-
-Vector.prototype.MultiplyByScalar = function( alpha )
-{
-  return new Vector( this.x * alpha, this.y * alpha );
-};
-
-Vector.prototype.Add = function( V )
-{
-  return new Vector( this.x + V.x, this.y + V.y );
-};
-
-Vector.prototype.DotProduct = function( V )
-{
-  return ( this.x * V.x ) + ( this.y * V.y );
-};
-
-Vector.prototype.GetColinearityCoeffs = function( V )
-{
-  return [ V.x / this.x, V.y / this.y ];
-};
-
-Vector.prototype.GetNorm = function()
-{
-  return Math.sqrt( Math.pow( this.x, 2 ) + Math.pow( this.y, 2 ) );
-};
 
 loopAngle = function( angle )
 {
